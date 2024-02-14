cde488fb131d72bd49dd0f7525ecfff8e1abf229
Sebastian <74075156+sebastianSSN@users.noreply.github.com>
2024-02-13 19:52:36 -0500

    Add files via upload

---
+++ miprogramadepython.py
@@ -0,0 +1,9 @@
+def main():
+    # Solicitar al usuario que ingrese su nombre
+    nombre = input("Por favor, ingresa tu nombre: ")
+    
+    # Mostrar un mensaje de bienvenida con el nombre del usuario
+    print("¡Hola, " + nombre + "! Bienvenido al programa.")
+
+if __name__ == "__main__":
+    main()
